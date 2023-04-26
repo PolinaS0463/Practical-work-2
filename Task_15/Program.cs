5e514dbd23bc55b0b50c9bc7aef6374a42df947c
Polina Semenova <polina-semenova-10@mail.ru>
2023-04-26 13:12:11 +0300

    Tasks completed

---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else if (day < 1 || day >7)
+{
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+    Console.WriteLine("Это не выходной день");
+}
